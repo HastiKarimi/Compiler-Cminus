db6b538a2b3209be0bcba5f26f02d770107fb692
Negar <babashahNegar@gmail.com>
2023-04-10 18:34:52 +0330

    Fix bugs

--- compiler.py
+++ compiler.py
@@ -6,8 +6,8 @@ import string
 
 class State:
 
-    def __init__(self, id: int, terminality_status: int, error_string: str = "empty", type_id: int = 0):
-        self.transitions = []
+    def __init__(self, id: int, terminality_status: int, error_string: str = "Invalid input", type_id: int = 0):
+        self.transitions = {}
         self.id = id
         self.error_str = error_string
         # terminality status:
@@ -17,16 +17,14 @@ class State:
         self.terminality_status = terminality_status
         self.type_id = type_id
 
-    def add_transition(self, ascii_ranges: [int, int], goal_state: int):
-        self.transitions.append((ascii_ranges, goal_state))
+    def add_transition(self, char: str, goal_state: int):
+        self.transitions[char] = goal_state
 
     def get_next_state(self, character: str) -> int:
-        for transition in self.transitions:
-            ascii_ranges, goal_state = transition
-            input_ascii = ord(character)
-            for (begin, end) in ascii_ranges:
-                if begin <= input_ascii <= end:
-                    return goal_state
+        if character in self.transitions:
+            return self.transitions[character]
+        elif "all" in self.transitions and character != "\0":
+            return self.id
         return -1
 
     def get_error(self, character: str = "") -> str:
@@ -43,7 +41,7 @@ class State:
 # digits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]  # 7
 char_groups = [[";", ":", ",", "[", "]", "(", ")", "{", "}", "+", "-", "<"], ["*"], ["="], ["/"],
                ["\n", "\r", "\t", "\v", "\f", " "], ["\0"], list(string.ascii_letters),
-               ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]]
+               ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"], ["all"]]
 
 
 def make_transition(chars_id: list[int], goal_states: list[int], state: State):
@@ -51,71 +49,11 @@ def make_transition(chars_id: list[int], goal_states: list[int], state: State):
         print("basi wrong")
 
     for i in range(len(chars_id)):
-        for char in char_groups[i]:
+        for char in char_groups[chars_id[i]]:
             state.add_transition(char, goal_states[i])
 
 
 class Scanner:
-    state_list = []
-    s = State(id=0, terminality_status=0, error_string="Invalid input")
-    make_transition(chars_id=[7, 5, 6, 0, 2, 3, 4, 1], goal_states=[1, 0, 3, 5, 6, 9, "?"],
-                    state=s)  # TODO add transition for *
-
-    state_list.append(s)
-
-    s = State(id=1, terminality_status=0, error_string="Invalid number")
-    make_transition(chars_id=[0, 1, 2, 3, 4, 5, 7], goal_states=[2, 2, 2, 2, 2, 0, 1],
-                    state=s)
-
-    state_list.append(s)
-
-    s = State(id=2, terminality_status=2, type_id=1)
-    state_list.append(s)
-
-    s = State(id=3, terminality_status=0, error_string="Invalid input")
-    make_transition(chars_id=[0, 1, 2, 3, 4, 5, 6, 7], goal_states=[4, 4, 4, 4, 4, 0, 3, 3],
-                    state=s)
-
-    s = State(id=4, terminality_status=2, type_id=2)
-    state_list.append(s)
-
-    s = State(id=5, terminality_status=1, type_id=4)
-    state_list.append(s)
-
-    s = State(id=6, terminality_status=0)
-    make_transition(chars_id=[0, 2, 3, 4, 6, 7], goal_states=[7, 8, 7, 7, 7, 7],
-                    state=s)
-    state_list.append(s)
-
-    s = State(id=7, terminality_status=2, type_id=4)
-    state_list.append(s)
-
-    s = State(id=8, terminality_status=1, type_id=4)
-    state_list.append(s)
-
-    s = State(id=9, terminality_status=0)
-    make_transition(chars_id=[0, 1, 2, 3, 4, 5, 6, 7], goal_states=[10, 13, 10, 10, 10, 10, 10, 10],
-                    state=s)
-    state_list.append(s)
-
-    s = State(id=10, terminality_status=2, type_id=4)
-    state_list.append(s)
-
-    s = State(id=13, terminality_status=0)
-    make_transition(chars_id=[0, 1, 2, 3, 4, 6, 7], goal_states=[13, 14, 13, 13, 13, 13, 13],
-                    state=s)
-    state_list.append(s)
-
-    s = State(id=14, terminality_status=0)
-    make_transition(chars_id=[0, 1, 2, 3, 4, 6, 7], goal_states=[13, 14, 13, 15, 13, 13, 13],
-                    state=s)
-    state_list.append(s)
-
-    s = State(id=15, terminality_status=1, type_id=5)
-    state_list.append(s)
-
-    s = State(id=16, terminality_status=1, type_id=6)
-    state_list.append(s)
 
     def __init__(self, input_file, output_file, lex_file, sym_file):
         self.input_file = input_file
@@ -123,36 +61,114 @@ class Scanner:
         self.lex_file = lex_file
         self.sym_file = sym_file
         self.types = {1: "NUM", 2: "ID", 3: "KEYWORD", 4: "SYMBOL", 5: "COMMENT", 6: "WHITESPACE"}
+        self.keywords = ["break", "else", "if", "int", "repeat", "return", "until", "void"]
 
         # elements of the symbol table - keywords should go first.
-        self.keywords = []
         self.identifiers = []
 
         self.line_number = 0
         self.current_line = ""
-        self.start_pnt = -1
+
+        # when we have a token in substring [i, i+1, ..., j], start pointer should be i and end pointer should be j
+        # if we want to read a character, we should read input[end pointer]
+        self.start_pnt = 0
         self.end_pnt = -1
         self.errors = []
+
+        self.state_list = []
+        self.make_state_list(state_list=self.state_list)
+
+    def make_state_list(self, state_list):
+        s = State(id=0, terminality_status=0)
+        make_transition(chars_id=[7, 5, 6, 0, 2, 3, 4, 1], goal_states=[1, 0, 3, 5, 6, 9, 16, 11],
+                        state=s)
+
+        state_list.append(s)
+
+        s = State(id=1, terminality_status=0, error_string="Invalid number")
+        make_transition(chars_id=[0, 1, 2, 3, 4, 5, 7], goal_states=[2, 2, 2, 2, 2, 0, 1],
+                        state=s)
+
+        state_list.append(s)
+
+        s = State(id=2, terminality_status=2, type_id=1)
+        state_list.append(s)
+
+        s = State(id=3, terminality_status=0)
+        make_transition(chars_id=[0, 1, 2, 3, 4, 5, 6, 7], goal_states=[4, 4, 4, 4, 4, 0, 3, 3],
+                        state=s)
+        state_list.append(s)
+
+        s = State(id=4, terminality_status=2, type_id=2)
+        state_list.append(s)
+
+        s = State(id=5, terminality_status=1, type_id=4)
+        state_list.append(s)
+
+        s = State(id=6, terminality_status=0)
+        make_transition(chars_id=[0, 1, 2, 3, 4, 6, 7], goal_states=[7, 7, 8, 7, 7, 7, 7],
+                        state=s)
+        state_list.append(s)
+
+        s = State(id=7, terminality_status=2, type_id=4)
+        state_list.append(s)
+
+        s = State(id=8, terminality_status=1, type_id=4)
+        state_list.append(s)
+
+        s = State(id=9, terminality_status=0)
+        make_transition(chars_id=[1], goal_states=[13],
+                        state=s)
+        state_list.append(s)
+
+        s = State(id=10, terminality_status=2, type_id=4)
+        state_list.append(s)
+
+        s = State(id=11, terminality_status=0, error_string="Unmatched comment")
+        make_transition(chars_id=[0, 1, 2, 4, 5, 6, 7], goal_states=[12, 12, 12, 12, 12, 12, 12],
+                        state=s)
+        state_list.append(s)
+
+        s = State(id=12, terminality_status=2, type_id=4)
+        state_list.append(s)
+
+        s = State(id=13, terminality_status=0, error_string="Unclosed comment")
+        make_transition(chars_id=[0, 1, 2, 3, 4, 6, 7, 8], goal_states=[13, 14, 13, 13, 13, 13, 13, 13],
+                        state=s)
+        state_list.append(s)
+
+        s = State(id=14, terminality_status=0, error_string="Unclosed comment")
+        make_transition(chars_id=[0, 1, 2, 3, 4, 6, 7], goal_states=[13, 14, 13, 15, 13, 13, 13],
+                        state=s)
+        state_list.append(s)
+
+        s = State(id=15, terminality_status=1, type_id=5)
+        state_list.append(s)
+
+        s = State(id=16, terminality_status=1, type_id=6)
+        state_list.append(s)
 
     # returns a tuple (next_char, line_updated)
     def get_next_char(self):
         line_updated = False
-        if self.line_number == 0 or self.end_pnt == len(self.current_line) - 1:
+        if self.line_number == 0 or self.end_pnt >= len(self.current_line) - 1:
             self.current_line = self.input_file.readline()
             if len(self.current_line) == 0:
-                return '\0'
+                # end of file
+                return '\0', line_updated
             self.end_pnt = -1
+            self.start_pnt = 0
             self.line_number += 1
             line_updated = True
 
-        if self.start_pnt == self.end_pnt:
-            self.start_pnt = self.end_pnt + 1
+        # if self.start_pnt == self.end_pnt:
+        #     self.start_pnt = self.end_pnt + 1
         self.end_pnt += 1
         return self.current_line[self.end_pnt], line_updated
 
     # if a type (id, number,...) is valuable for parser, returns true, else false
     @staticmethod
-    def is_type_parsable(self, type_id: int):
+    def is_type_parsable(type_id: int):
         return type_id not in [5, 6]
 
     # output:
@@ -162,13 +178,15 @@ class Scanner:
         state_id = 0
         while self.state_list[state_id].terminality_status == 0:
             next_char, line_updated = self.get_next_char()
+            if next_char == "\n":
+                pass
             next_state_id = self.state_list[state_id].get_next_state(next_char)
             # the id of eof state is 0
             if next_state_id == 0:
                 return None
             if next_state_id == -1:
                 self.handle_error(state_id, next_char)
-                self.start_pnt = self.end_pnt
+                self.start_pnt = self.end_pnt + 1
                 return self.get_next_token()
 
             state_id = next_state_id
@@ -179,7 +197,14 @@ class Scanner:
         lexeme = self.current_line[self.start_pnt: self.end_pnt + 1]
         type_id = self.state_list[state_id].type_id
 
-        if self.is_type_parsable(type_id):
+        # todo clean it up
+        if type_id == 2:
+            if lexeme in self.keywords:
+                type_id = 3
+
+        self.start_pnt = self.end_pnt + 1
+
+        if Scanner.is_type_parsable(type_id):
             token = self.types[type_id], lexeme
             self.install_in_symbol_table(token)
             if write_to_file:
@@ -192,16 +217,14 @@ class Scanner:
     def handle_error(self, state_id: int, char: str):
         lexeme = self.current_line[self.start_pnt: self.end_pnt + 1]
         self.errors.append([self.line_number, lexeme, self.state_list[state_id].get_error()])
+        print("error!!!     " + self.state_list[state_id].get_error() + " the lexeme is " + lexeme)
         # self.lex_file.write(self.state_list[state_id].get_error())
 
     def install_in_symbol_table(self, token):
         keyword_type = "KEYWORD"
         id_type = "ID"
         type, lexeme = token
-        if type == keyword_type:
-            if lexeme not in self.keywords:
-                self.keywords.append(lexeme)
-        elif type == id_type:
+        if type == id_type:
             if lexeme not in self.identifiers:
                 self.identifiers.append(lexeme)
 
@@ -212,10 +235,10 @@ class Scanner:
         index = 0
         for keyword in self.keywords:
             index += 1
-            text += index + index_separator + keyword + "\n"
+            text += str(index) + index_separator + keyword + "\n"
         for identifier in self.identifiers:
             index += 1
-            text += index + index_separator + identifier + "\n"
+            text += str(index) + index_separator + identifier + "\n"
 
         self.sym_file.truncate(0)
         self.sym_file.seek(0)
@@ -229,7 +252,7 @@ class Scanner:
         if line_updated:
             if self.line_number != 0:
                 text += "\n"
-            text += self.line_number + ".\t"
+            text += str(self.line_number) + ".\t"
         else:
             text += " "
         text += "(" + type + ", " + lexeme + ")"
@@ -252,6 +275,7 @@ scanner = Scanner(
 
 while True:
     token = scanner.get_next_token()
+    print(token)
     if token is None:
         break
 
@@ -259,8 +283,3 @@ in_file.close()
 out_file.close()
 lex_file.close()
 sym_file.close()
-
-
-# todo
-def add_states(scanner: Scanner):
-    pass
