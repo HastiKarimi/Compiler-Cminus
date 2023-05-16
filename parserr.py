3cb98e82e88e3c23eb420972c326323b6fb3fa31
hasti <hastikarimi2002@gmail.com>
2023-05-16 20:13:39 +0330

    add: parser class
    Co-authored-by: Negar Babashah <Negarbsh@users.noreply.github.com>

--- parserr.py
+++ parserr.py
@@ -1,65 +1,3 @@
-import json
-
-data = {}
-with open("data.json", "r") as f:
-    data = json.load(f)
-
-
-production_rules_file = open("rules.txt", "r")
-production_rule_lines = production_rules_file.readlines()
-
-non_terminals = {}
-rule_index = 0
-rules = []
-
-for production_rule in production_rule_lines:
-    nt, right_side = production_rule.split("->")
-    nt = nt.strip()
-    right_side = right_side.strip().split("|")
-    nt_rule_list = []
-    for rule in right_side:
-        the_rule = Rule(rule_index, rule.strip().split(" "))
-        rules.append(the_rule)
-        nt_rule_list.append(rule_index)
-        rule_index += 1
-
-    non_terminals[nt] = Nonterminal(nt, nt_rule_list)
-    
-    
-
-
-
-
-# non_terminal = {"Define": NoneTerminal()}
-
-# class NoneTerminal:
-#     def __init__(self, name, rules):
-#         self.name = name
-#         rules = [1, 5, 8] 
-#         first
-#         follow
-    
-
-# nt -> a1a2a3 | b1b2b3 | ...
-# a1 -> asdf | 
-
-
-
-# def find_first:
-
-
-
-# class Rule:
-#     nt_or_terminals = ['a1', 'a2', 'a3']
-#     first = []
-#     use():
-#         a1()
-#         a2()
-#         a3()
-    
-
-
-
 # 1. initialize 
 # 2. get first non_terminal
 # 3. get a token
@@ -68,28 +6,98 @@ for production_rule in production_rule_lines:
 # 6. in cases of error, panic mode
 
 
+import json
 
+
+rules = []
+non_terminals = {}
+parser = Parser(non_terminals, rules)
+
+
+class Parser:
+    def __init__(self, non_teminals_dict, rules_list) -> None:
+        self.data = {}
+        self.non_terminals = non_teminals_dict
+        self.rules = rules_list
+        self.initialize()
+        self.current_token = None
+        self.current_nt = self.non_terminals["Program"]
+
+
+    def initialize(self):
+        with open("data.json", "r") as f:
+            self.data = json.load(f)
+
+
+        production_rules_file = open("rules.txt", "r")
+        production_rule_lines = production_rules_file.readlines()
+
+        
+        rule_index = 0
+
+        for production_rule in production_rule_lines:
+            nt, right_side = production_rule.split("->")
+            nt = nt.strip()
+            right_side = right_side.strip().split("|")
+            nt_rule_list = []
+            for rule in right_side:
+                the_rule = Rule(rule_index, rule.strip().split(" "))
+                self.rules.append(the_rule)
+                nt_rule_list.append(rule_index)
+                rule_index += 1
+
+            self.non_terminals[nt] = Nonterminal(nt, nt_rule_list)
+
+    def run(self):
+        pass
+
+    def call_nt(self, nt_name: str):
+        self.current_nt = non_terminals[nt_name]
+        rule = self.current_nt.predict_rule(self.current_token)
+        # todo in parse tree, set the actions of rule as the children of current_nt
+
+
+
+    def find_nt_firsts(self, nt_name:str) -> list[str]:
+        pass # todo implement
+
+    def find_nt_follows(self, nt_name: str) -> list[str]:
+        pass # todo implement
+
+    def find_rule_firsts(self, rule_id: int) -> list[str]:
+        pass # todo implement
+
+    def is_terminal(self, name: str) -> bool:
+        pass # todo implement
+
+    def match_token(sefl, terminal: str):
+        pass # todo implement
+        
+    
 
 class Rule:
     def __init__(self, rule_id: int, actions: list[str]):
         self.actions = actions
         self.id = rule_id
-        self.firsts = find_rule_firsts(rule_id)
+        self.firsts = parser.find_rule_firsts(rule_id)
 
     def apply(self):
         for action in self.actions:
-            if is_terminal(action):
-                match_token(action)
+            if parser.is_terminal(action):
+                parser.match_token(action)
+            else:
+                parser.call_nt(action)
+                
         
 
 class Nonterminal:
     def __init__(self, name: str, rule_ids: list[int]):
         self.name = name
         self.rule_ids = rule_ids
-        self.firsts = find_nt_firsts(name)
-        self.follows = find_nt_follows(name)
+        self.firsts = parser.find_nt_firsts(name)
+        self.follows = parser.find_nt_follows(name)
 
-    def predict_rule(self) -> int:
+    def predict_rule(self, current_token: str) -> int:
         # predicts the id of the rule to apply based on the current token
         pass
     
@@ -100,17 +108,3 @@ class Nonterminal:
         # todo error recovery process
 
 
-def find_nt_firsts(nt_name:str) -> list[str]:
-    pass # todo implement
-
-def find_nt_follows(nt_name: str) -> list[str]:
-    pass # todo implement
-
-def find_rule_firsts(rule_id: int) -> list[str]:
-    pass # todo implement
-
-def is_terminal(name: str) -> bool:
-    pass # todo implement
-
-def match_token(terminal: str):
-    pass # todo implement
