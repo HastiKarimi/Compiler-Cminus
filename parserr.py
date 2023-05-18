0253e7647a6005c4374094eaa236d74b8bce3baf
Negar <babashahNegar@gmail.com>
2023-05-18 16:02:22 +0330

    Implement panic mode
    - Fix epsilon rule usage in rule prediction
    - Write syntax errors in file

--- parserr.py
+++ parserr.py
@@ -21,11 +21,14 @@ def is_terminal(name: str) -> bool:
 
 
 class Parser:
-    def __init__(self, non_terminals_dict, rules_list) -> None:
+    def __init__(self, non_terminals_dict, rules_list, errors_file) -> None:
         self.non_terminals = non_teminals_dict
         self.rules = rules_list
+        self.errors_file = errors_file
         self.initialize()
         self.current_token = None
+        self.current_line = None
+        self.scanner = None  # todo
         self.current_nt = self.non_terminals["Program"]
         self.parse_tree = []
 
@@ -61,7 +64,16 @@ class Parser:
         my_list = nt_list
         self.current_nt = non_terminals[nt_name]
         rule = self.current_nt.predict_rule(self.current_token)
-        # TODO if no rule was predicted go to panic mode
+        if rule is None:
+            if self.current_token in self.current_nt.follows:
+                self.report_missing_token(self.current_token.name, self.current_line)
+                return  # assume that the current nt is found and we should continue
+            else:
+                self.report_illegal_token(self.current_token, self.current_line)
+                self.update_token()  # assume there was an illegal input and ignore it
+                self.call_nt(nt_name, nt_list)
+                return
+
         my_list.extend(rule.get_actions())
         for i in range(len(my_list)):
             action = my_list[i]
@@ -73,10 +85,21 @@ class Parser:
                 self.call_nt(action, child_nt_list)
 
     def match_action(self, terminal_action: str):
-        if self.current_token == terminal_action:
-            self.current_token = scanner.get_next_token()
+        # TODO what happens if current token is $?
+        if self.current_token is not terminal_action:
+            self.report_missing_token(self.current_token, self.current_line)
+        self.update_token()
 
-        # TODO else go to panic mode
+    def update_token(self):
+        self.current_token, self.current_line = self.scanner.get_next_token()
+
+    def report_missing_token(self, missing_token_name: str, line_number: int):
+        error_message = "#" + str(line_number) + " : syntax error, missing " + str(missing_token_name)
+        self.errors_file.write(error_message)
+
+    def report_illegal_token(self, illegal_token: str, line_number: int):
+        error_message = "#" + str(line_number) + " : syntax error, illegal " + str(missing_token_name)
+        self.errors_file.write(error_message)
 
 
 class Rule:
@@ -98,21 +121,23 @@ class Nonterminal:
         self.rule_ids = rule_ids
         self.firsts = data[first_keyword][self.name]
         self.follows = data[follow_keyword][self.name]
-        self.has_epsilon_rule = False
+        self.epsilon_rule = None
         for i in rule_ids:
             rule_firsts = self.find_rule_firsts(i)
-            if not self.has_epsilon_rule:
-                self.has_epsilon_rule = epsilon_keyword in rule_firsts
+            if not self.has_epsilon_rule and epsilon_keyword in rule_firsts:
+                self.epsilon_rule = i
             rules[i].set_first(rule_firsts)
 
     def find_rule_firsts(self, rule_id: int) -> list[str]:
         rule = rules[rule_id]
 
         if rule.get_actions()[0] == epsilon_keyword:  # the rule itself is epsilon
-            return data[follow_keyword][self.name]
+            return rule.get_actions
 
         rule_first = []
-        for action in rule.get_actions():
+        actions = rule.get_actions()
+        for index in range(len(actions)):
+            action = actions[index]
             if is_terminal(action):
                 rule_first.append(action)
                 return remove_duplicates(rule_first)
@@ -120,7 +145,12 @@ class Nonterminal:
                 # then action is a non-terminal
                 action_first = data[first_keyword][action]
                 if epsilon_keyword in action_first:
-                    rule_first = action_first.remove(epsilon_keyword) + rule_first
+                    if index is not len(action) - 1:
+                        rule_first += action_first.remove(epsilon_keyword)
+                    else:
+                        # If we're here, all the actions were terminals that contained epsilon in their firsts.
+                        # So epsilon must be included in rule_first
+                        rule_first += action_first
                 else:
                     rule_first = action_first + rule_first
                     remove_duplicates(rule_first)
@@ -133,7 +163,8 @@ class Nonterminal:
             rule = rules[rule_id]
             if current_token in rule.firsts:
                 return rule_id
-        return None
+        return self.epsilon_rule
+        # it's either None or one of the rules that has epsilon in its first set
 
 
 rules = []
@@ -142,4 +173,5 @@ data = {}
 epsilon_keyword = 'EPSILON'
 first_keyword = 'first'
 follow_keyword = 'follow'
-parser = Parser(non_terminals, rules)
+errors_file = open("syntax_errors.txt", "w+")
+parser = Parser(non_terminals, rules, errors_file)
